1f7975f4b37eafcfac8b597c462d6c322888956e
Diogo Guerra <dy090.guerra@gmail.com>
2025-06-23 22:22:58 +0200

    FileProcessingStatus: add get_data_status method for data_id object
    Signed-off-by: Diogo Guerra <dy090.guerra@gmail.com>

--- cognee/modules/data/methods/get_data.py
+++ cognee/modules/data/methods/get_data.py
@@ -3,6 +3,7 @@ from typing import Optional
 from cognee.infrastructure.databases.relational import get_relational_engine
 from ..exceptions import UnauthorizedDataAccessError
 from ..models import Data
+from ..models.Data import FileProcessingStatus
 
 
 async def get_data(user_id: UUID, data_id: UUID) -> Optional[Data]:
@@ -26,3 +27,23 @@ async def get_data(user_id: UUID, data_id: UUID) -> Optional[Data]:
             )
 
         return data
+
+
+async def get_data_status(user_id: UUID, data_id: UUID
+                          ) -> Optional[FileProcessingStatus]:
+    """get data_id current processing_status.
+
+    Args:
+        user_id (UUID): user ID
+        data_id (UUID): ID of the data to retrieve
+
+    Returns:
+        Optional[FileProcessingStatus]: That status of the requested data
+        object if found, None otherwise
+    """
+    data = await session.get(Data, data_id)
+
+    if data is not None:
+        return data.proocessing_status
+
+    return None
